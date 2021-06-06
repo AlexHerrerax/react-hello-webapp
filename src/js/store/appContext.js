266db722fb4033dceb2e760b2f00a1911901417d
Alex <axherrerax@gmail.com>
2021-06-06 04:11:30 -0400

    cambio en context

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import getState from "./flux.js";
 
 
-
+export const Context = React.createContext(null);
 const injectContext = PassedComponent => {
 	const StoreWrapper = props => {
-		
+		//this will be passed as the contenxt value
 		const [state, setState] = useState(
 			getState({
 				getStore: () => state.store,
@@ -22,7 +22,6 @@ const injectContext = PassedComponent => {
 			
 			state.actions.loadData();
 		}, []);
-
 		return (
 			<Context.Provider value={state}>
 				<PassedComponent {...props} />
