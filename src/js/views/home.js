750c4b8cdfa189d393c8bb97bff4f8ed7d39c7c3
Alex Herrera <axherrerax@gmail.com>
2021-06-06 07:45:00 +0000

    subida de todo

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { CardCharacters } from "../component/cardCharacters";
+import { CardPlanets } from "../component/cardPlanets";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container text-font-base">
+			<h2 className="text-danger">Characters</h2>
+			<div className="testimonial-group">
+				<div className="row">
+					{store.people.map((person, i) => (
+						<div className="cardhorizontal" key={i}>
+							<CardCharacters PeopleID={person.uid} />
+						</div>
+					))}
+				</div>
+			</div>
+			<h2 className="text-danger mt-3">Planets</h2>
+			<div className="testimonial-group">
+				<div className="row">
+					{store.planets.map((planet, i) => (
+						<div className="cardhorizontal" key={i}>
+							<CardPlanets PlanetID={planet.uid} />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -1,24 +1,26 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
-import { Context } from "../store/appContext";
+import { useParams } from "react-router-dom";
+
+import { DetailCharacters } from "../component/detailCharacter";
+import { DetailPlannet } from "../component/detailPlanet";
 
 export const Single = props => {
-	const { store, actions } = useContext(Context);
 	const params = useParams();
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
 
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+	if (params.type === "people") {
+		return (
+			<div>
+				<DetailCharacters PeopleID={params.theid} />
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<DetailPlannet PlanetID={params.theid} />
+			</div>
+		);
+	}
 };
 
 Single.propTypes = {
